1ef54a1258321e35c511a1167368355d361ed506
Alexander Menk <alex@addismap.com>
2023-05-01 17:10:52 +0200

    Only cache-clear once per key per request

--- app/Services/Cache.php
+++ app/Services/Cache.php
@@ -17,14 +17,19 @@ class Cache
     public static function remember(string $key, \Closure $callback): mixed
     {
         static $logged = false;
+        static $flushedKeys = [];
 
         if (request()->header('cache-control') === 'no-cache') {
             if (!$logged) {
                 Log::notice(sprintf('Cache flush header received from %s', request()->ip()));
                 $logged = true;
             }
-            CacheFacade::forget($key);
+            if (!isset($flushedKeys[$key])) {
+                CacheFacade::forget($key);
+                $flushedKeys[$key] = true;  // avoid flushing the same key twice in a request
+            }
         }
+
         return CacheFacade::remember($key, self::LIFETIME, $callback);
     }
 }
