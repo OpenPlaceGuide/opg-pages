616ed3cde0494fcb9c2adacf94e087950889e788
Alexander Menk <a.menk@imi.de>
2025-01-05 17:47:40 +0100

    Remove opening hours rendering because cache
    * Page is cached, so doing this in PHP does not make any sense

--- app/Services/TagRenderer.php
+++ app/Services/TagRenderer.php
@@ -28,7 +28,6 @@ class TagRenderer
         foreach ($tags as $key=>$value) {
             if ($key === 'opening_hours') {
                 $key = 'Opening Times';
-                $value = $value . ' - ' . self::parseOpeningHours($value);
                 $lines[] = $key . ": " . $value;
                 continue;
             }
@@ -40,62 +39,6 @@ class TagRenderer
         }
 
         return $lines;
-    }
-
-    private function parseOpeningHours($value)
-    {
-        $timeZone = $this->getTimeZone($this->osmInfo->lat, $this->osmInfo->lon);
-
-        $now = (new \DateTimeImmutable('now'))->setTimezone(new DateTimeZone($timeZone));
-        $hours = OsmStringToOpeningHoursConverter::openingHoursFromOsmString($value);
-        if ($hours->isOpenAt($now)) {
-            return 'currently open';
-        } else {
-            return 'currently (' . $now->format('Y-m-d H:i:s') . ' local time) closed, next open: ' . $hours->nextOpen($now)->format('Y-m-d H:i:s');
-        }
-    }
-
-    /**
-     * @source https://stackoverflow.com/a/15535345
-     */
-    private function getTimeZone($cur_lat, $cur_long, $country_code = '')
-    {
-        $timezone_ids = ($country_code) ? DateTimeZone::listIdentifiers(DateTimeZone::PER_COUNTRY, $country_code)
-            : DateTimeZone::listIdentifiers();
-
-        if($timezone_ids && is_array($timezone_ids) && isset($timezone_ids[0])) {
-
-            $time_zone = '';
-            $tz_distance = 0;
-
-            //only one identifier?
-            if (count($timezone_ids) == 1) {
-                $time_zone = $timezone_ids[0];
-            } else {
-
-                foreach($timezone_ids as $timezone_id) {
-                    $timezone = new DateTimeZone($timezone_id);
-                    $location = $timezone->getLocation();
-                    $tz_lat   = $location['latitude'];
-                    $tz_long  = $location['longitude'];
-
-                    $theta    = $cur_long - $tz_long;
-                    $distance = (sin(deg2rad($cur_lat)) * sin(deg2rad($tz_lat)))
-                        + (cos(deg2rad($cur_lat)) * cos(deg2rad($tz_lat)) * cos(deg2rad($theta)));
-                    $distance = acos($distance);
-                    $distance = abs(rad2deg($distance));
-                    // echo '<br />'.$timezone_id.' '.$distance;
-
-                    if (!$time_zone || $tz_distance > $distance) {
-                        $time_zone   = $timezone_id;
-                        $tz_distance = $distance;
-                    }
-
-                }
-            }
-            return  $time_zone;
-        }
-        return 'unknown';
     }
 
     private function queryTagInfo($key, $value)
